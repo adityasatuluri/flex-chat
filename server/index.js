76e031c080817369bf3eff243210b1ecc589691f
aditya.satuluri <s.aditya.in@gmail.com>
2025-09-21 13:01:10 +0530

    patch2.0

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const databaseURL = process.env.DATABASE_URL;
 
 app.use(
   cors({
-    origin: process.env.ORIGIN, // Ensure this matches the client origin
+    origin: "http://localhost:5174", // must match frontend
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
